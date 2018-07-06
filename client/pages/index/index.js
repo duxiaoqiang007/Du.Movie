3c57d6ead7491979c5fc2d0687c44e60b6d80990
duxiaoqiang007 <821786625@qq.com>
2018-07-06 14:42:50 +0800

    修改主页的按钮‘‘我的’的功能

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,15 +1,25 @@
-//index.js
+const app = getApp()
 var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
 var util = require('../../utils/util.js')
-
 Page({
   data: {
     homeMovie: {},
-    homeComment:{}
+    homeComment:{},
+    userInfo:null
   },
   onLoad() {
     this.getMovie()
+  },
+  onShow(){
+    app.checkSession({
+      success: ({ userInfo }) => {
+        this.setData({
+          userInfo: userInfo
+        })
+        console.log(userInfo)
+      }
+    })
   },
   getMovie() {
     wx.showLoading({
@@ -76,6 +86,50 @@ Page({
   onTapHotMovie(){
     wx.navigateTo({
       url: '../hotMovies/hotMovies',
+    })
+  },
+  onTapMy(){
+    if(!this.data.userInfo){
+      wx.showModal({
+        title: '提示',
+        content: '您尚未登陆，是否登陆？',
+        success: res => {
+          if (res.confirm) {
+            this.login()
+          }
+          else {
+            wx.showToast({
+              icon:'none',
+              title: '登陆失败',
+            })
+          }
+        },
+        fail:res=>{
+          console.log(res)
+          wx.showToast({
+            icon: 'none',
+            title: '登陆失败',
+          })
+        }
+      })
+    }
+    else{
+      wx.navigateTo({
+        url: '../loveComment/loveComment',
+      })  
+    }
+  },
+  login(){
+    app.login({
+      success: ({ userInfo }) => {
+        this.setData({
+          userInfo: userInfo
+        })
+        console.log(userInfo)
+        wx.navigateTo({
+          url: '../loveComment/loveComment',
+        })
+      }
     })
   }
 })
