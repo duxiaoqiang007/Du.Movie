9e93705edf373bbf8fe649577ee1773325f6cbf0
duxiaoqiang007 <821786625@qq.com>
2018-07-06 11:40:23 +0800

    电影首页，还没有评论

---
+++ client/pages/index/index.js
@@ -0,0 +1,71 @@
+//index.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+Page({
+  data: {
+    homeMovie: {}
+    // homeComment:{}
+  },
+  onLoad() {
+    this.getMovie()
+    // this.getComment()
+  },
+  getMovie() {
+    wx.showLoading({
+      title: '热门电影加载中...',
+    })
+    qcloud.request({
+      url: config.service.homeMovie,
+      success: res => {
+        wx.hideLoading()
+        console.log(res.data.data)
+        if (!res.data.code) {
+          this.setData({
+            homeMovie: res.data.data[0]
+          })
+        }
+        else {
+          wx.showToast({
+            title: '热门电影加载失败',
+          })
+        }
+      },
+      error: res => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '热门电影加载失败',
+        })
+      }
+    })
+  },
+  // getComment(){
+  //   wx.showLoading({
+  //     title: '评论加载中...',
+  //   })
+  //   qcloud.request({
+  //     url: config.service.homeComment,
+  //     success: res => {
+  //       wx.hideLoading()
+  //       console.log(res.data.data)
+  //       if (!res.data.code) {
+  //         this.setData({
+  //           homeComment: res.data.data[0]
+  //         })
+  //       }
+  //       else {
+  //         wx.showToast({
+  //           title: '评论加载失败',
+  //         })
+  //       }
+  //     },
+  //     error: res => {
+  //       wx.hideLoading()
+  //       wx.showToast({
+  //         title: '评论加载失败',
+  //       })
+  //     }
+  //   })   
+  // }
+})
---
+++ server/controllers/home.js
@@ -0,0 +1,10 @@
+const DB = require('../units/db.js')
+
+module.exports = {
+  homeMovie: async ctx => {
+    ctx.state.data = await DB.query("select * from movies where movies.id = 1")
+  },
+  homeComment: async ctx => {
+    ctx.state.data = await DB.query("SELECT user.nickName,user.avatarUrl, comment.comment_type,comment.comment from user,comment WHERE user.open_id = (SELECT comment.user FROM comment WHERE movie_id =1 LIMIT 0,1)")
+  }
+}
